a2eadca8dcdcdcb88d50f0a248c00a23519088ca
ukeh <ukeh.hari@gmail.com>
2025-05-15 20:08:06 +0530

    updatd

--- cronJobs/jobs.js
+++ cronJobs/jobs.js
@@ -182,96 +182,97 @@ exports.closeExpiredAgents = async () => {
                 const airdropId = latestAirdrop ? latestAirdrop.airdropId + 1 : 1;
 
                 let recipients = await agentChatService.getAgentChatSummary({ agentId: agentId });
-
-                let claims = recipients.map(({ senderAddress, attempts }, index) => {
-                    return {
-                        address: senderAddress,
-                        amount: attempts * tokenAmount,
-                        order: index,
-                        airdropId: airdropId
-                    }
-                });
-
-                const totalAmount = claims.reduce((sum, { amount }) => {
-                    return sum + amount;
-                }, 0);
-
-                const totalRecipients = claims.length;
-
-
-
-                let success = false;
-                let session;
-
-                try {
-                    const airdrop = {
-                        airdropId: airdropId,
-                        totalRecipients: totalRecipients,
-                        totalTokens: totalAmount
-                    }
-
-                    session = await mongoose.startSession();
-
-                    await session.withTransaction(async () => {
-                        await airdropService.createSessionAirdrop(airdrop, { session });
-                        await claimService.createClaims(claims, { session });
-                        await agentService.findAgentAndUpdate({ agentId: agentId, isOver: true }, { airdropId: airdropId });
+                if (recipients.length > 0) {
+                    let claims = recipients.map(({ senderAddress, attempts }, index) => {
+                        return {
+                            address: senderAddress,
+                            amount: attempts * tokenAmount,
+                            order: index,
+                            airdropId: airdropId
+                        }
                     });
-                    success = true;
-                    console.log(`Agent: ${agent.agentId} airdrop data created offchain-db with airdropId ${airdropId}`);
 
-                } catch (error) {
-                    console.log(error);
-                } finally {
-                    session.endSession();
-                }
+                    const totalAmount = claims.reduce((sum, { amount }) => {
+                        return sum + amount;
+                    }, 0);
 
-                if (success) {
+                    const totalRecipients = claims.length;
+
+
+
+                    let success = false;
+                    let session;
+
                     try {
-
-                        // do actual contract airdrop txn
-
-                        let airdropIdHex = Web3.utils.numberToHex(airdropId);
-                        airdropIdHex = Web3.utils.padLeft(airdropIdHex, 64);
-
-                        const mekleData = claims.map(({ address, amount, order, airdropId }) => {
-                            return {
-                                address: address,
-                                amount: Web3.utils.toWei(amount, 'ether'),
-                                proofIndex: order,
-                                airdropId: airdropIdHex
-                            }
-                        });
-
-                        const { root } = await merkleTreeService.buildMerkleTree(mekleData);
-
-                        const totalAmountToWei = Web3.utils.toWei(totalAmount, 'ether');
-
-                        const gasEstimate = await airdropAdminContract.methods.createAirdrop(airdropIdHex, root, totalRecipients, totalAmountToWei).estimateGas({ from: airdropAdminAccount.address });
-                        const currentGasPrice = await airdropAdminWeb3.eth.getGasPrice();
-                        const priorityFee = Web3.utils.toWei('3', 'gwei');
-                        const totalGasPrice = currentGasPrice + BigInt(priorityFee);
-
-                        await airdropAdminContract.methods
-                            .createAirdrop(airdropIdHex, root, totalRecipients, totalAmountToWei)
-                            .send({
-                                from: airdropAdminAccount.address,
-                                gas: gasEstimate.toString(),
-                                gasPrice: totalGasPrice.toString(),
-                            });
-
-                        console.log(`Agent: ${agent.agentId} airdrop created onchain with airdropId ${airdropId}`);
-
-                        // mark airdrop as created 
-
-                        const airdropResult = await airdropService.findAirdropAndUpdate({ airdropId: airdropId, isCreated: false }, { isCreated: true });
-                        if (airdropResult) {
-                            await claimService.findclaimsAndUpdate({ airdropId: airdropId, isCreated: false }, { isCreated: true });
-                            console.log(`Agent: ${agent.agentId} airdrop marked as created on offchain-DB with airdropId ${airdropId}`);
+                        const airdrop = {
+                            airdropId: airdropId,
+                            totalRecipients: totalRecipients,
+                            totalTokens: totalAmount
                         }
 
+                        session = await mongoose.startSession();
+
+                        await session.withTransaction(async () => {
+                            await airdropService.createSessionAirdrop(airdrop, { session });
+                            await claimService.createClaims(claims, { session });
+                            await agentService.findAgentAndUpdate({ agentId: agentId, isOver: true }, { airdropId: airdropId });
+                        });
+                        success = true;
+                        console.log(`Agent: ${agent.agentId} airdrop data created offchain-db with airdropId ${airdropId}`);
+
                     } catch (error) {
-                        console.log(error)
+                        console.log(error);
+                    } finally {
+                        session.endSession();
+                    }
+
+                    if (success) {
+                        try {
+
+                            // do actual contract airdrop txn
+
+                            let airdropIdHex = Web3.utils.numberToHex(airdropId);
+                            airdropIdHex = Web3.utils.padLeft(airdropIdHex, 64);
+
+                            const mekleData = claims.map(({ address, amount, order, airdropId }) => {
+                                return {
+                                    address: address,
+                                    amount: Web3.utils.toWei(amount, 'ether'),
+                                    proofIndex: order,
+                                    airdropId: airdropIdHex
+                                }
+                            });
+
+                            const { root } = await merkleTreeService.buildMerkleTree(mekleData);
+
+                            const totalAmountToWei = Web3.utils.toWei(totalAmount, 'ether');
+
+                            const gasEstimate = await airdropAdminContract.methods.createAirdrop(airdropIdHex, root, totalRecipients, totalAmountToWei).estimateGas({ from: airdropAdminAccount.address });
+                            const currentGasPrice = await airdropAdminWeb3.eth.getGasPrice();
+                            const priorityFee = Web3.utils.toWei('3', 'gwei');
+                            const totalGasPrice = currentGasPrice + BigInt(priorityFee);
+
+                            await airdropAdminContract.methods
+                                .createAirdrop(airdropIdHex, root, totalRecipients, totalAmountToWei)
+                                .send({
+                                    from: airdropAdminAccount.address,
+                                    gas: gasEstimate.toString(),
+                                    gasPrice: totalGasPrice.toString(),
+                                });
+
+                            console.log(`Agent: ${agent.agentId} airdrop created onchain with airdropId ${airdropId}`);
+
+                            // mark airdrop as created 
+
+                            const airdropResult = await airdropService.findAirdropAndUpdate({ airdropId: airdropId, isCreated: false }, { isCreated: true });
+                            if (airdropResult) {
+                                await claimService.findclaimsAndUpdate({ airdropId: airdropId, isCreated: false }, { isCreated: true });
+                                console.log(`Agent: ${agent.agentId} airdrop marked as created on offchain-DB with airdropId ${airdropId}`);
+                            }
+
+                        } catch (error) {
+                            console.log(error)
+                        }
                     }
                 }
 
